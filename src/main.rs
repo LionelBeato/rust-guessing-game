2f77214439908cedae88c35041644a596ba315bc
Lionel Beato <beato@lionel.works>
2020-05-27 18:22:58 -0400

    first commit

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+
+// Welcome to the wonderful world of Rust!
+
+// Like many C-like languages, Rust needs an entrypoint in order for it to run.
+// Otherwise, the program will not do anything!
+// this entry point is known as a "function"
+// 
 fn main() {
     println!("Hello, world!");
 }
