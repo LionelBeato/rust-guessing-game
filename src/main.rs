34a1e747d4b87aeeb743462aed975c5b75e56fd4
Lionel Beato <beato@lionel.works>
2020-06-01 18:11:04 -0400

    finalizes guessing game

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,39 @@
+use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
 
-// Welcome to the wonderful world of Rust!
-
-// Like many C-like languages, Rust needs an entrypoint in order for it to run.
-// Otherwise, the program will not do anything!
-// this entry point is known as a "function"
-// 
 fn main() {
     println!("Hello, world!");
+
+    println!("Guess the number!");
+
+    let secret_number = rand::thread_rng().gen_range(1, 101);
+
+    println!("The secret number is: {}", secret_number);
+
+    println!("Please input your guess.");
+
+    loop {
+        let mut guess = String::new();
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line!");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        println!("You guessed: {}", guess);
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too Small!"),
+            Ordering::Greater => println!("Too Big!"),
+            Ordering::Equal => { 
+                println!("You win!");
+                break; 
+            }
+        }
+    }
 }
